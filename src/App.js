bc82ac7cac61f759551713e8a25ae0964ed68295
Brianoksoftware <brianokdev@gmail.com>
2023-10-02 12:03:53 +0300

    added a <p> tag to App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <p>
+          Brian Kabonyo
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +21,7 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
     </div>
   );
