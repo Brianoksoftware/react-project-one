2e54de7b9655001a352a779db894e7ede9041048
Brianoksoftware <brianokdev@gmail.com>
2023-10-02 14:38:09 +0300

    added <h4>I'm on branch branch-one</h4>

--- src/App.js
+++ src/App.js
@@ -6,22 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <p>
-          Brian Kabonyo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
+        <h4>I'm on branch branch-one</h4>
       </header>
     </div>
   );
