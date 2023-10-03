7ecdb8fa8026372c92c1d72cbd88e15fe3226c6e
Brianoksoftware <brianokdev@gmail.com>
2023-10-03 14:47:19 +0300

    Added a Posts component to fetch and return post data, modified the css styling in App.css and globally in index.css

---
+++ src/components/Posts.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import axios from "axios";
+//import {useState, useEffect} from 'react';
+
+function Posts (){
+    const[posts, setPosts] = React.useState([])
+
+    React.useEffect(() => {
+        axios.get("http://jsonplaceholder.typicode.com/posts").then((response) => setPosts(response.data));
+    }, []);
+
+    return(
+        <ul className="posts">
+            {posts.map((post) =>(
+                <li className="post" key={post.id}>
+                    <h4>{post.title}</h4>
+                    <p>{post.body}</p>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default Posts;
